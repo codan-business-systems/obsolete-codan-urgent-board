bffc48577e7900be38042bbfa69191fbbbbc6d8c
Peter Roehlen <peter.roehlen@codan.com.au>
2019-03-12 09:27:46 +0000

    Client side search/filtering implemented

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,77 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/m/MessageBox"
+], function (Controller, JSONModel, Filter, FilterOperator, MessageBox) {
 	"use strict";
 
 	return Controller.extend("codan.zurgentboard.controller.Main", {
-		onInit: function () {
 
+		_searchSettingsPopover: null,
+		
+		onInit: function () {
+			// View model for view state
+			this._oViewModel = new JSONModel({
+				search: {
+					value: "",
+					fields: [
+						{ text: "Part Number", selected: true, path: "material" },
+						{ text: "Part Description", selected: true, path: "description" },
+						{ text: "Order Id", selected: true, path: "objectkey" },
+						{ text: "Contact", selected: true, path: "enteredByName" }
+					]
+				}
+			});
+			this.getView().setModel(this._oViewModel, "viewModel");
+		},
+		
+		toggleSearchSettings: function(oEvent) {
+			var oPopover = this.byId("searchSettingsPopover");
+			if (oPopover.isOpen()) {
+				oPopover.close();
+			} else {
+				oPopover.openBy(oEvent.getSource());
+			}
+		},
+		
+		onSearch: function() {
+			var aSearchFields = this._oViewModel.getProperty("/search/fields");
+			var sSearchValue = this._oViewModel.getProperty("/search/value");
+			
+			// Build filters for each active search field
+			var aAllFilters = [];
+			if (sSearchValue) {
+				var aFieldFilters = aSearchFields
+					.filter(function (oSearchField){
+						return oSearchField.selected;
+					})
+					.map(function (oSearchField){
+						return new Filter({
+							path: oSearchField.path,
+							operator: FilterOperator.Contains,
+							value1: sSearchValue
+						});
+				});
+				
+				// If no field filters active, advise user that nothing will be selected
+				if (!aFieldFilters.length) {
+					MessageBox.warning("Nothing will be found because no search fields have been selected");
+				}
+				
+				// Combine filters with OR statement not AND
+				var oCombinedFilter = new Filter({
+					filters: aFieldFilters,
+					and: false
+				});
+				aAllFilters.push(oCombinedFilter);
+			}
+			
+			// Apply filter
+			var oTable = this.byId("tableMain");
+			var oBinding = oTable.getBinding("items");
+			oBinding.filter(aAllFilters);
 		}
 	});
 });
